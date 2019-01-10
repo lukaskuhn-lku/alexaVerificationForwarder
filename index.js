2e141ca624ded23da41ac963ecc67cdc147dbab0
tu674 <lukas.kuhn@deutsche-boerse.com>
2019-01-10 16:06:28 +0100

    Added request post but deactivated for now

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ exports.index = (event, context, callback) => {
     console.log("Headers: " + JSON.stringify(event.headers));
     console.log("Body: " + JSON.stringify(event.body));
 
+    console.log(JSON.stringify(event.req));
+
     console.log("Starting Verification");
     verifier(event.headers["SignatureCertChainUrl"], event.headers["Signature"], event.body, function (error) {
         console.log("Verification failed.");
@@ -14,5 +16,16 @@ exports.index = (event, context, callback) => {
     var url = "https://ffmw0rps5g.execute-api.eu-central-1.amazonaws.com/product-12-2018";
 
     console.log("VERIFICATION SUCCESFUL");
+
+    /*
+    request.post({
+       headers: JSON.stringify(event.headers),
+       url: url,
+       body: JSON.stringify(event.body)
+    }, function(error,response,body){
+
+    });
+    */
+
 
 };
