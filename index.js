cc442b74f7e491b533f0af8bf43b09603ea03f3b
tu674 <lukas.kuhn@deutsche-boerse.com>
2019-01-10 15:36:28 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const verifier = require('alexa-verifier');
+const request = require('request');
+
+
+exports.index = (event, context, callback) => {
+    console.log("Headers: " + JSON.stringify(event.headers));
+    console.log("Body: " + JSON.stringify(event.body));
+
+    console.log("Starting Verification");
+    verifier(event.headers["SignatureCertChainUrl"], event.headers["Signature"], event.body, function (error) {
+        console.log("Verification failed.");
+    });
+
+    var url = "https://ffmw0rps5g.execute-api.eu-central-1.amazonaws.com/product-12-2018";
+
+    console.log("VERIFICATION SUCCESFUL");
+
+};
