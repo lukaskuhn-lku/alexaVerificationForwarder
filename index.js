c1b83ba9be6e582925b6f2cafc2a6cc51761825e
tu674 <lukas.kuhn@deutsche-boerse.com>
2019-01-11 10:41:29 +0100

    Functional Version, changed promises and use callback instead. Needed to parse the response body to JSON before sending it back

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 
 const index = function(event, context, callback) {
-    console.log("[Index Handler] Request received")
+    console.log("[Index Handler] Request received");
 
     var signatureCertChainURL = event.headers["SignatureCertChainUrl"];
     var signature = event.headers["Signature"];
@@ -11,39 +11,38 @@ const index = function(event, context, callback) {
 
     console.log("SignatureCertChainUrl: " + signatureCertChainURL);
     console.log("Signature: " + signature);
-    console.log("Body: " + body)
+    console.log("Body: " + body);
 
     console.log("Starting Verification process");
 
-    return new Promise( (resolve) => {
-        verifier(signatureCertChainURL, signature, body, function (error) {
-            if (error) {
-                console.log("Alexa Skill Verification Request failed.");
-                console.log(error.toString());
-                resolve(error);
-            } else {
-                 let url = "https://ffmw0rps5g.execute-api.eu-central-1.amazonaws.com/product-12-2018";
+    verifier(signatureCertChainURL, signature, body, function (error) {
+        if (error) {
+            console.log("Alexa Skill Verification Request failed.");
+            console.log(error.toString());
 
-                 console.log("Alexa Skill Verification Request successful");
+            return context.fail("Bad Request: Failed Authentication");
+        } else {
+            let url = "https://ffmw0rps5g.execute-api.eu-central-1.amazonaws.com/product-12-2018";
 
-                 resolve(new Promise((resolve) => {
-                    var response = request.post({
-                        headers: JSON.stringify(event.headers),
-                        url: url,
-                        body: JSON.stringify(event.body)
-                    }, function (error, response, body) {
-                        if (error) {
-                            console.log(error);
-                            throw error;
-                        }
+            console.log("Alexa Skill Verification Request successful");
 
-                        console.log("Promise: " + response.body);
-                        resolve(body);
-                    });
-                 }));
-            }
-        });
+            request.post({
+                headers: JSON.stringify(event.headers),
+                url: url,
+                body: JSON.stringify(event.body)
+            }, function (error, response, body) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                }
+
+                console.log("Promise: " + response.body);
+                console.log("Body: " + body);
+                callback(null, JSON.parse(response.body));
+            });
+        }
     });
+
 };
 
 exports.handler = index;
