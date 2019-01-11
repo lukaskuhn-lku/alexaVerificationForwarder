b1f6c4dde9d0026d4927f0ad2d9a64be58b7b647
tu674 <lukas.kuhn@deutsche-boerse.com>
2019-01-11 09:13:49 +0100

    Added promises and Verification process

--- index.js
+++ index.js
@@ -2,30 +2,48 @@ const verifier = require('alexa-verifier');
 const request = require('request');
 
 
-exports.index = (event, context, callback) => {
-    console.log("Headers: " + JSON.stringify(event.headers));
-    console.log("Body: " + JSON.stringify(event.body));
+const index = function(event, context, callback) {
+    console.log("[Index Handler] Request received")
 
-    console.log(JSON.stringify(event.req));
+    var signatureCertChainURL = event.headers["SignatureCertChainUrl"];
+    var signature = event.headers["Signature"];
+    var body = JSON.stringify(event.body);
 
-    console.log("Starting Verification");
-    verifier(event.headers["SignatureCertChainUrl"], event.headers["Signature"], event.body, function (error) {
-        console.log("Verification failed.");
+    console.log("SignatureCertChainUrl: " + signatureCertChainURL);
+    console.log("Signature: " + signature);
+    console.log("Body: " + body)
+
+    console.log("Starting Verification process");
+
+    return new Promise( (resolve) => {
+        verifier(signatureCertChainURL, signature, body, function (error) {
+            if (error) {
+                console.log("Alexa Skill Verification Request failed.");
+                console.log(error.toString());
+                resolve(error);
+            } else {
+                 let url = "https://ffmw0rps5g.execute-api.eu-central-1.amazonaws.com/product-12-2018";
+
+                 console.log("Alexa Skill Verification Request successful");
+
+                 resolve(new Promise((resolve) => {
+                    var response = request.post({
+                        headers: JSON.stringify(event.headers),
+                        url: url,
+                        body: JSON.stringify(event.body)
+                    }, function (error, response, body) {
+                        if (error) {
+                            console.log(error);
+                            throw error;
+                        }
+
+                        console.log("Promise: " + response.body);
+                        resolve(body);
+                    });
+                 }));
+            }
+        });
     });
-
-    var url = "https://ffmw0rps5g.execute-api.eu-central-1.amazonaws.com/product-12-2018";
-
-    console.log("VERIFICATION SUCCESFUL");
-
-    /*
-    request.post({
-       headers: JSON.stringify(event.headers),
-       url: url,
-       body: JSON.stringify(event.body)
-    }, function(error,response,body){
-
-    });
-    */
-
-
 };
+
+exports.handler = index;
